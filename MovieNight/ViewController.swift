47d19fa325823c8879eb0eb27a1b1796b288f135
Erik Carlson <noirelkscar@gmail.com>
2018-11-30 11:58:35 -0800

    Initial Commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Erik Carlson on 11/30/18.
+//  Copyright © 2018 Round and Rhombus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
